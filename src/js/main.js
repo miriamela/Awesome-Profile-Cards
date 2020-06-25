a5f8f876964ce0f3fea2c0a38cb04f885529d721
claudiasanes <claudiasanes@gmail.com>
2020-06-25 14:26:15 +0200

    rem function

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line quotes
+"use strict";
+
+// eslint-disable-next-line no-unused-vars
+function rem(pixels) {
+  // eslint-disable-next-line no-undef
+  return (pixels / $defaultFontSize) * 1;
+}
+
+/* ejemplo de uso de esta función:
+
+p {
+    font-size: rem(18);
+}
+
+*/
