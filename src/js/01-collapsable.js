aad58feddfca12288909dd26b6522ead62c4bf58
Lucia Martinez <luci.dmartinez@gmail.com>
2020-06-29 13:57:44 +0200

    last minute fixes pre demo

--- src/js/01-collapsable.js
+++ src/js/01-collapsable.js
@@ -1,35 +1,38 @@
-'use strict';
+"use strict";
 
 //SELECTORS...
-const angleDownButton1 = document.querySelector('.js-angleDown1');
-const designSection = document.querySelector('.js-design');
+const angleDownButton1 = document.querySelector(".js-angleDown1");
+const designSection = document.querySelector(".js-design");
 
-const angleDownButton2 = document.querySelector('.js-angleDown2');
-const formSection = document.querySelector('.js-formData');
+const angleDownButton2 = document.querySelector(".js-angleDown2");
+const formSection = document.querySelector(".js-formData");
 
-const angleDownButton3 = document.querySelector('.js-angleDown3');
-const createSection = document.querySelector('.js-share');
+const angleDownButton3 = document.querySelector(".js-angleDown3");
+const createSection = document.querySelector(".js-share");
 
 /*const createButton = document.querySelector('.create');
 const shareSection = document.querySelector('.js-createText');*/
 
 //FUNCTIONS
 
-function displayOrHideContent() {
-  designSection.classList.toggle('hidden');
+function displayOrHideContent(event) {
+  designSection.classList.toggle("hidden");
+  event.currentTarget.classList.toggle("rot");
 }
-function displayOrHideContent2() {
-  formSection.classList.toggle('hidden');
+function displayOrHideContent2(event) {
+  formSection.classList.toggle("hidden");
+  event.currentTarget.classList.toggle("rot");
 }
-function displayOrHideContent3() {
-  createSection.classList.toggle('hidden');
+function displayOrHideContent3(event) {
+  createSection.classList.toggle("hidden");
+  event.currentTarget.classList.toggle("rot");
 }
 /*function displayOrHideShareSection() {
   shareSection.classList.toggle('hidden');
 }*/
 
 //LISTENERS
-angleDownButton1.addEventListener('click', displayOrHideContent);
-angleDownButton2.addEventListener('click', displayOrHideContent2);
-angleDownButton3.addEventListener('click', displayOrHideContent3);
+angleDownButton1.addEventListener("click", displayOrHideContent);
+angleDownButton2.addEventListener("click", displayOrHideContent2);
+angleDownButton3.addEventListener("click", displayOrHideContent3);
 /*createButton.addEventListener('click', displayOrHideShareSection);*/
--- src/js/02-card.js
+++ src/js/02-card.js
@@ -1,110 +1,117 @@
-'use strict';
+"use strict";
 
 //COLOR PALLETE
 
 //SELECTORS...
-const coldPalette = document.querySelector('.cold-palette');
-const hotPalette = document.querySelector('.hot-palette');
-const mixPalette = document.querySelector('.mix-palette');
-const cardName = document.querySelector('.js-name');
-const cardPosition = document.querySelector('.js-job');
-const socialMediaIcons = document.querySelector('.js-socialMedia-icons');
+const coldPalette = document.querySelector(".cold-palette");
+const hotPalette = document.querySelector(".hot-palette");
+const mixPalette = document.querySelector(".mix-palette");
+const cardName = document.querySelector(".js-name");
+const cardPosition = document.querySelector(".js-job");
+const socialMediaIcons = document.querySelectorAll(".js-socialMedia-icons");
 
 //FUNCTIONS
 function ChangeToColdPalette() {
-  if (cardName.classList.contains('red-name')) {
-    cardName.classList.remove('red-name');
-    cardName.classList.add('cold-name');
-  } else if (cardName.classList.contains('mix-name')) {
-    cardName.classList.remove('mix-name');
-    cardName.classList.add('cold-name');
+  if (cardName.classList.contains("red-name")) {
+    cardName.classList.remove("red-name");
+    cardName.classList.add("cold-name");
+  } else if (cardName.classList.contains("mix-name")) {
+    cardName.classList.remove("mix-name");
+    cardName.classList.add("cold-name");
   }
-  if (cardPosition.classList.contains('red-job')) {
-    cardPosition.classList.remove('red-job');
-    cardPosition.classList.add('cold-job');
-  } else if (cardPosition.classList.contains('mix-job')) {
-    cardPosition.classList.remove('mix-job');
-    cardPosition.classList.add('cold-job');
+  if (cardPosition.classList.contains("red-job")) {
+    cardPosition.classList.remove("red-job");
+    cardPosition.classList.add("cold-job");
+  } else if (cardPosition.classList.contains("mix-job")) {
+    cardPosition.classList.remove("mix-job");
+    cardPosition.classList.add("cold-job");
   }
-  if (socialMediaIcons.classList.contains('red-icon')) {
-    socialMediaIcons.classList.remove('red-icon');
-    socialMediaIcons.classList.add('cold-icon');
-  } else if (socialMediaIcons.classList.contains(' mix-icon')) {
-    socialMediaIcons.classList.remove('mix-icon');
-    socialMediaIcons.classList.add('cold-icon');
+  for (const mediaIcon of socialMediaIcons) {
+    if (mediaIcon.classList.contains("red-icon")) {
+      mediaIcon.classList.remove("red-icon");
+      mediaIcon.classList.add("cold-icon");
+    } else if (mediaIcon.classList.contains(" mix-icon")) {
+      mediaIcon.classList.remove("mix-icon");
+      mediaIcon.classList.add("cold-icon");
+    }
   }
 }
 function ChangeToHotPalette() {
-  if (cardName.classList.contains('cold-name')) {
-    cardName.classList.remove('cold-name');
-    cardName.classList.add('red-name');
-  } else if (cardName.classList.contains('mix-name')) {
-    cardName.classList.remove('mix-name');
-    cardName.classList.add('red-name');
+  if (cardName.classList.contains("cold-name")) {
+    cardName.classList.remove("cold-name");
+    cardName.classList.add("red-name");
+  } else if (cardName.classList.contains("mix-name")) {
+    cardName.classList.remove("mix-name");
+    cardName.classList.add("red-name");
   }
-  if (cardPosition.classList.contains('cold-job')) {
-    cardPosition.classList.remove('cold-job');
-    cardPosition.classList.add('red-job');
-  } else if (cardPosition.classList.contains('mix-job')) {
-    cardPosition.classList.remove('mix-job');
-    cardPosition.classList.add('red-job');
+  if (cardPosition.classList.contains("cold-job")) {
+    cardPosition.classList.remove("cold-job");
+    cardPosition.classList.add("red-job");
+  } else if (cardPosition.classList.contains("mix-job")) {
+    cardPosition.classList.remove("mix-job");
+    cardPosition.classList.add("red-job");
   }
-  if (socialMediaIcons.classList.contains('cold-icon')) {
-    socialMediaIcons.classList.remove('cold-icon');
-    socialMediaIcons.classList.add('red-icon');
-  } else if (socialMediaIcons.classList.contains(' mix-icon')) {
-    socialMediaIcons.classList.remove('mix-icon');
-    socialMediaIcons.classList.add('red-icon');
+  for (const mediaIcon of socialMediaIcons) {
+    console.log("WOrking?");
+    if (mediaIcon.classList.contains("cold-icon")) {
+      mediaIcon.classList.remove("cold-icon");
+      mediaIcon.classList.add("red-icon");
+    } else if (mediaIcon.classList.contains(" mix-icon")) {
+      mediaIcon.classList.remove("mix-icon");
+      mediaIcon.classList.add("red-icon");
+    }
   }
 }
 
 function ChangeToMixPalette() {
-  if (cardName.classList.contains('cold-name')) {
-    cardName.classList.remove('cold-name');
-    cardName.classList.add('mix-name');
-  } else if (cardName.classList.contains('red-name')) {
-    cardName.classList.remove('red-name');
-    cardName.classList.add('mix-name');
+  if (cardName.classList.contains("cold-name")) {
+    cardName.classList.remove("cold-name");
+    cardName.classList.add("mix-name");
+  } else if (cardName.classList.contains("red-name")) {
+    cardName.classList.remove("red-name");
+    cardName.classList.add("mix-name");
   }
-  if (cardPosition.classList.contains('cold-job')) {
-    cardPosition.classList.remove('cold-job');
-    cardPosition.classList.add('mix-job');
-  } else if (cardPosition.classList.contains('red-job')) {
-    cardPosition.classList.remove('red-job');
-    cardPosition.classList.add('mix-job');
+  if (cardPosition.classList.contains("cold-job")) {
+    cardPosition.classList.remove("cold-job");
+    cardPosition.classList.add("mix-job");
+  } else if (cardPosition.classList.contains("red-job")) {
+    cardPosition.classList.remove("red-job");
+    cardPosition.classList.add("mix-job");
   }
-  if (socialMediaIcons.classList.contains('cold-icon')) {
-    socialMediaIcons.classList.remove('cold-icon');
-    socialMediaIcons.classList.add('mix-icon');
-  } else if (socialMediaIcons.classList.contains(' red-icon')) {
-    socialMediaIcons.classList.remove('red-icon');
-    socialMediaIcons.classList.add('mix-icon');
+  for (const mediaIcon of socialMediaIcons) {
+    if (mediaIcon.classList.contains("cold-icon")) {
+      mediaIcon.classList.remove("cold-icon");
+      mediaIcon.classList.add("mix-icon");
+    } else if (mediaIcon.classList.contains(" red-icon")) {
+      mediaIcon.classList.remove("red-icon");
+      mediaIcon.classList.add("mix-icon");
+    }
   }
 }
 
 //LISTENERS
-coldPalette.addEventListener('click', ChangeToColdPalette);
-hotPalette.addEventListener('click', ChangeToHotPalette);
-mixPalette.addEventListener('click', ChangeToMixPalette);
+coldPalette.addEventListener("click", ChangeToColdPalette);
+hotPalette.addEventListener("click", ChangeToHotPalette);
+mixPalette.addEventListener("click", ChangeToMixPalette);
 
 //////////////////////////////////////////////////////////
 
 //DATA FORM
 
 //SELECTORS...
-const userNameElem = document.querySelector('.js-userName');
-const userJobElem = document.querySelector('.js-userJob');
-const userPhoneNumberElem = document.querySelector('.js-phoneNumber');
-const userEmailElem = document.querySelector('.js-email');
-const userLinkedinElem = document.querySelector('.js-linkedin');
-const userGithubElem = document.querySelector('.js-github');
+const userNameElem = document.querySelector(".js-userName");
+const userJobElem = document.querySelector(".js-userJob");
+const userPhoneNumberElem = document.querySelector(".js-phoneNumber");
+const userEmailElem = document.querySelector(".js-email");
+const userLinkedinElem = document.querySelector(".js-linkedin");
+const userGithubElem = document.querySelector(".js-github");
 const userData = {};
-const renderedUserName = document.querySelector('.js-userName-result');
-const renderedUserJob = document.querySelector('.js-userJob-result');
-const renderedUserPhoneNumber = document.querySelector('.js-phone-result');
-const renderedUserEmail = document.querySelector('.js-email-result');
-const renderedUserLinkedin = document.querySelector('.js-linkedin-result');
-const renderedUserGithub = document.querySelector('.js-github-result');
+const renderedUserName = document.querySelector(".js-userName-result");
+const renderedUserJob = document.querySelector(".js-userJob-result");
+const renderedUserPhoneNumber = document.querySelector(".js-phone-result");
+const renderedUserEmail = document.querySelector(".js-email-result");
+const renderedUserLinkedin = document.querySelector(".js-linkedin-result");
+const renderedUserGithub = document.querySelector(".js-github-result");
 
 //FUNCTIONS...
 function getInputValues() {
@@ -128,9 +135,9 @@ function updateImputHandler() {
   renderUserInfo(userData);
 }
 //LISTENERS...
-userNameElem.addEventListener('keyup', updateImputHandler);
-userJobElem.addEventListener('keyup', updateImputHandler);
-userPhoneNumberElem.addEventListener('keyup', updateImputHandler);
-userEmailElem.addEventListener('keyup', updateImputHandler);
-userLinkedinElem.addEventListener('keyup', updateImputHandler);
-userGithubElem.addEventListener('keyup', updateImputHandler);
+userNameElem.addEventListener("keyup", updateImputHandler);
+userJobElem.addEventListener("keyup", updateImputHandler);
+userPhoneNumberElem.addEventListener("keyup", updateImputHandler);
+userEmailElem.addEventListener("keyup", updateImputHandler);
+userLinkedinElem.addEventListener("keyup", updateImputHandler);
+userGithubElem.addEventListener("keyup", updateImputHandler);
